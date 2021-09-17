ae791832f24cae7063354d2965877e5263d22318
gocharlie <gu.yinqi1023@gmail.com>
2021-09-17 09:11:39 +0000

    script for bgp routing

---
+++ network_coding/summary_bgp.py
@@ -0,0 +1,36 @@
+from netmiko import ConnectHandler
+from netaddr import IPNetwork
+
+def iosg_router01():
+    router01 = {
+        'device_type': 'cisco_ios',
+        'ip': '10.81.3.51',
+        'username': 'admin',
+        'password': 'admin'
+    }
+    return router01
+
+def parse_device(**kwargs):
+    with ConnectHandler(**kwargs) as connect:
+        connect.send_command('terminal length 0')
+        output = connect.send_command('show ip bgp')
+        connect.send_command('terminal length 10')
+        # return output
+        print(output)
+
+def strip_routes(routes):
+    route_list = []
+    for route in routes:
+        if route.get('network') == '0.0.0.0':
+            print(route)
+            continue
+        route = route.get('network') + '/' + route.get('mask')
+        print(route)
+        route_list.append(IPNetwork(route))
+    return route_list
+
+if __name__ == "__main__":
+    R1 = iosg_router01()
+    router = parse_device(**R1)
+    # print(strip_routes(router))
+    print(router)
